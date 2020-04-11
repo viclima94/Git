9efd97737118b8a8f9fa3a9800a8dbee96fc4626
victorlima <viclima94@outlook.com>
2020-04-11 13:03:14 -0300

    chore: Commit inicial

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require ('express')
+
+const app = express()
+
+app.use(express(json))
+
+
+app.get('/teste',(req,res ) =>{
+  return res.json({hello:'world'})
+});
+
+
+app.listen(3333)
